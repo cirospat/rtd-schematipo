554dc52744328d82b009c74aad330a423ca7b5a7
pablopersico <36224887+pablopers@users.noreply.github.com>
2018-04-26 10:49:41 +0200

    add arrow.js file
    add js folder with arrow.js file

---
+++ js/arrow.js
@@ -0,0 +1,14 @@
+<!-- script Back To Top
+-->
+<a id='backTop'>Back To Top</a>
+<script src="top/jquery.backTop.min.js"></script>
+<script>
+          $(document).ready( function() 
+              $('#backTop').backTop({
+                  'position' : 500,
+                  'speed' : 500,
+                  'color' : 'red',
+
+                                           });
+           });
+</script>
