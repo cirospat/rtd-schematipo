b533efc1c90ff4d33b6fe2160c2b8c020aef4b33
ciro <cirospat@gmail.com>
2018-04-26 13:38:56 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -30,6 +30,9 @@ html_static_path = ['static']
 def setup(app):
     # overrides for wide tables in RTD theme
     app.add_stylesheet('theme_overrides.css') # path relative to static
+    app.add_javascript('js/arrow.js')
+    app.add_stylesheet('top/backTop.css')
+    app.add_stylesheet('top/jquerysctipttop.css')
 
 """
   You might want to uncomment the “latex_documents = []” if you use CKJ characters in your document.
